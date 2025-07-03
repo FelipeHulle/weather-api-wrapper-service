7fc57f526f615fea940e3db78f0c01ad490bb390
Felipe Hulle <felipehulle@hotmail.com>
2025-07-02 23:19:17 -0300

    improving check if redis and api is available

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #%%
 from weather_client import WeatherService
+from flask import Flask
 
-location = input('Digite a cidade ou endereço: ')
 
 
 client = WeatherService()
 
-data = client.get_location_temperature(location)
-print(f'Em {data['address']} no dia {data['today']} temos temperatura maxima de {data['temp_max']}°C e minima de {data['temp_min']}°C')
+
+data = client.get_location_temperature('vila velha')
+data
--- weather_client.py
+++ weather_client.py
@@ -19,18 +19,37 @@ class WeatherClient:
         
         endpoint = f'{self._BASE_URL}/{location_cleaned}?key={self._weather_key}&unitGroup=metric'
 
+
         response = requests.get(endpoint)
-        
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return response.raise_for_status()
-        
+
+        try:
+            response = requests.get(endpoint)
+            
+            if response.status_code == 200:
+                return {
+                    'status' : True,
+                    'data'   : response.json()
+                    }
+            else:
+              response.raise_for_status()   
+        except requests.exceptions.RequestException as e:
+            return {
+                'status' : False,
+                'data'   : f'Erro {e}'
+            }
+
 class RedisCache:
     # Escrever comandos basicos do redis aqui e interação entre redis e service no weatherservice
     def __init__(self):
         self._pool = redis.ConnectionPool(host=host_redis,port=port_redis,db=0)
         self._redis = redis.Redis(connection_pool=self._pool)
+
+    def _is_running(self):
+        try:
+            self._redis.ping()
+            return True
+        except Exception as e:
+            return False
 
     def set_data(self,key,value):
         self._redis.set(key, value, ex=60, nx=True)
@@ -43,46 +62,41 @@ class RedisCache:
     def exists_key(self,key):
         response = self._redis.exists(key)
         return response
-    
+
 class WeatherService:
 
     def __init__(self, client: WeatherClient = WeatherClient(), cache: RedisCache = RedisCache()):
         self.client = client
         self.cache = cache
+        self._redis_running = cache._is_running()
 
     def get_location_temperature(self,location: str):
 
-        if self.cache.exists_key(location):
-            return self.cache.get_data(location)
-        else:
-            data = self.client.get_weather(location)
-            address = data['resolvedAddress']
-            today = data['days'][0]['datetime']
-            temp_max = data['days'][0]['tempmax']
-            temp_min = data['days'][0]['tempmin']
+        if self._redis_running:
 
-            data_json = {
-                'address' : address,
-                'today' : today,
-                'temp_max' : temp_max,
-                'temp_min' : temp_min
+            if self.cache.exists_key(location):
+                return self.cache.get_data(location)
+                    
+        data = self.client.get_weather(location)
+                
+        if not data.get('status'):
+            return {
+                'error': data.get('data')
             }
 
+        data_cleaned = data.get('data')
+        address = data_cleaned['resolvedAddress']
+        today = data_cleaned['days'][0]['datetime']
+        temp_max = data_cleaned['days'][0]['tempmax']
+        temp_min = data_cleaned['days'][0]['tempmin']
+        data_json = {
+            'address' : address,
+            'today' : today,
+            'temp_max' : temp_max,
+            'temp_min' : temp_min
+        }
+
+        if self._redis_running:
             self.cache.set_data(location,json.dumps(data_json))
-
-            return self.cache.get_data(location)
-
         
-#%%
-if __name__ == '__main__': 
-
-    # cache = WeatherService(WeatherClient(),RedisCache())
-    # data = cache.get_location('london')
-    # print(data)
-
-    client = WeatherService(WeatherClient(),RedisCache())
-    
-    data = client.get_location_temperature('vila velha')
-    print(data)
-    # a = client.get_weather('vila velha')
-    # print(a)
+        return data_json
